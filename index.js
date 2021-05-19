ea99b87e889b8672d5090eb16f3b0b140de994b3
Shiragu <pipboy2009@yandex.ru>
2021-05-19 11:42:03 +0300

    renderbox added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var $start = document.querySelector("#start");
 var $game = document.querySelector("#game");
+var score = 0;
 
 $start.addEventListener("click", gameStart);
+$game.addEventListener("click", handleBoxClick);
 
 function gameStart() {
   $game.style.backgroundColor = "#fff";
@@ -9,4 +11,24 @@ function gameStart() {
   renderBox();
 }
 
-function renderBox() {}
+function handleBoxClick(event) {
+  if (event.target.dataset.box) {
+    score++;
+    renderBox();
+  }
+}
+
+function renderBox() {
+  $game.innerHTML = "";
+  var $box = document.createElement("div");
+
+  $box.style.height = $box.style.width = "50px";
+  $box.style.position = "absolute";
+  $box.style.backgroundColor = "#000";
+  $box.style.top = "50px";
+  $box.style.left = "70px";
+  $box.style.cursor = "pointer";
+  $box.setAttribute("data-box", true);
+
+  $game.insertAdjacentElement("afterbegin", $box);
+}
